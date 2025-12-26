54d297b82d17ba7f54cccfbb068194e636d78a57
“ravikumar9555” <ravikumar7518989121@email.com>
2025-12-26 11:05:00 +0530

    Basic Setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const bodyParser = require('body-parser');
+const {PORT} = require('../src/config/serverConfig')
+const setUpAndStartServer = ()=>{
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT, ()=>{
+        console.log(`Server started at ${PORT}`);
+    })
+}
+
+setUpAndStartServer();
